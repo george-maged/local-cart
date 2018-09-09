46b58b39cd6ebd39d3582b7582c0bf380bd1fefa
george-maged <georgemaged9129@gmail.com>
2018-09-09 22:10:45 +0200

    replace placeholder image with lorempixel

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -5,8 +5,8 @@ class Product extends Component {
   render() {
     return (
       <div className={"product "+this.props.productClass}>
-        {/* <img src={this.props.product.image} alt={this.props.product.title}/> */}
-        <img src={"https://imgplaceholder.com/420x320/ff7f7f/333333/fa-image"} alt={this.props.product.title}/>
+        <img src={this.props.product.image} alt={this.props.product.title}/>
+        {/* <img src={"https://imgplaceholder.com/420x320/ff7f7f/333333/fa-image"} alt={this.props.product.title}/> */}
         <div className="product__price">
           {this.props.product.price} EGP
         </div>
